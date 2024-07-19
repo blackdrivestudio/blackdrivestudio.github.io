2532b11ee232ea08c81cdb0ab4921ac66d380150
blackdrivestudio <blackdrivestudio@gmail.com>
2024-07-19 08:38:18 +0300

    Update parse_and_combine.py

--- parse_and_combine.py
+++ parse_and_combine.py
@@ -2,19 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import os
 
-# URLs to fetch
-url1 = "https://raw.githubusercontent.com/PME-Crew/app-ads.txt/master/app-ads.txt"
-url2 = "https://cas.ai/app-ads.txt"
-
 def fetch_text(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     return soup.get_text()
 
 def combine_texts(text1, text2):
-    return text1 + "\n#\n" + text2
+    return text1 + "#\n" + text2
 
 def main():
+    url1 = os.getenv('URL1')
+    url2 = os.getenv('URL2')
+    
+    if not url1 or not url2:
+        print("URLs are not set properly.")
+        return
+    
     text1 = fetch_text(url1)
     text2 = fetch_text(url2)
     combined_text = combine_texts(text1, text2)
