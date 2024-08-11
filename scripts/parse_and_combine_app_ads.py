f59464a365729bb7adba3241c7dd94117de36a84
blackdrivestudio <blackdrivestudio@gmail.com>
2024-08-11 13:37:31 +0300

    Update parse_and_combine_app_ads.py

--- scripts/parse_and_combine_app_ads.py
+++ scripts/parse_and_combine_app_ads.py
@@ -11,20 +11,15 @@ def combine_texts(texts):
     return "#\n".join(texts)
 
 def main():
-    urls = []
-    index = 1
-    while True:
-        url = os.getenv(f'APP_ADS_URL{index}')
-        if not url:
-            break
-        urls.append(url)
-        index += 1
+    urls = os.getenv('APP_ADS_URLS')
     
     if not urls:
         print("No URLs are set properly.")
         return
     
-    texts = [fetch_text(url) for url in urls]
+    urls = urls.split(',')
+    
+    texts = [fetch_text(url.strip()) for url in urls]
     combined_text = combine_texts(texts)
 
     output_file = "app-ads.txt"
